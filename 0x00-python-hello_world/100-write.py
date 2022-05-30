bfe9c945755635b0bb825199efefd79fba1712d3
EWanjau <wamwanjau@gmail.com>
2022-05-30 17:37:37 +0000

    update file

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 import sys
-
-def print_to_stderr(*a):
-    print(*a, file = sys.stderr)
-
-print_to_stderr("and that piece of art is useful - Dora Korpar, 2015-10-19")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
