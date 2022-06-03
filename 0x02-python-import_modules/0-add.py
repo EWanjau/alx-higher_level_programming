02189f4c6ab142c25a20dbc5da16c5463aab712a
EWanjau <wamwanjau@gmail.com>
2022-06-03 09:41:09 +0000

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-from add_0 import add as add1
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add1(a, b)))
+from add_0 import add
+
+if __name__== "__main__":
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
