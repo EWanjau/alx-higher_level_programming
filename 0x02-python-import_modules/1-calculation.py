311f4e384a046379bcb3feab85a67ac2ea9c308a
EWanjau <wamwanjau@gmail.com>
2022-06-03 12:22:30 +0000

    update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,6 +2,13 @@
 
 from calculator_1 import *
 if __name__ == "__main__":
+    """Display function
+    arguments:
+        a: first int
+        b: 2nd int
+    Returns:
+        the value calculated by functions
+        """
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
