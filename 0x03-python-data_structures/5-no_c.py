495823a63f61e48cd01f9645b5a1ee728e4ab24d
EWanjau <wamwanjau@gmail.com>
2022-06-06 13:25:01 +0000

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 
 def no_c(my_string):
     str_list = list(my_string)
-    for i in range(len(str_list) + 1):
-        if 'C' == str_list[i]:
+    for i in range(len(str_list)):
+        if str_list[i] == 'C':
             del(str_list[i])
     new_str = "".join(str_list)
     return new_str
