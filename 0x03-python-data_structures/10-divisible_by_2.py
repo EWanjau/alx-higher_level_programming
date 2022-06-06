f5fd837817637dbebdb75ca1723492edf56a6777
EWanjau <wamwanjau@gmail.com>
2022-06-06 21:09:59 +0000

    divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+    new_list = []
     for i in range(len(my_list)):
         if i % 2 == 0:
-            print("{:d} is divisible by 2".format(my_list[i]))
+            new_list.append(True)
         else:
-            print("{:d} is not divisible by 2".format(my_list[i]))
+            new_list.append(False)
+    return new_list
