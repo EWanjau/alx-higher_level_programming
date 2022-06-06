aadeb3129d1a9ae729f682bc61d941182384918e
EWanjau <wamwanjau@gmail.com>
2022-06-06 19:28:32 +0000

    remove letters

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    str_list = list(my_string)
-    for i in range(len(str_list)):
-        if str_list[i] == 'C':
-            del(str_list[i])
-    new_str = "".join(str_list)
-    return new_str
+    new_str = [x for x in my_string if x != 'c' and x != 'C']
+    return "".join(new_str)
