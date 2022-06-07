c540a12b3c88abba4fd89611a057dfe18a43575f
EWanjau <wamwanjau@gmail.com>
2022-06-07 10:28:08 +0000

    woo

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,5 +3,9 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
+    else:
+        highest = my_list[0]
     for i in range(len(my_list)):
-        if
+        if my_list[i] > highest:
+            highest = my_list[i]
+    return max
