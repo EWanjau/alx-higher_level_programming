3d05912e543fa71098c5ea5ddad18a86d4b77861
EWanjau <wamwanjau@gmail.com>
2022-06-08 21:09:49 +0000

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,16 +9,16 @@ if __name__ == "__main__":
     if count != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    num1 = int(sys.argv[1])
-    num2 = int(sys.argv[3])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     if (sys.argv[2] is op[0]):
-        print("{:d} {:s} {:d} = {:d}".format(num1, op[0], num2, add(num1, num2)))
+        print("{:d} {:s} {:d} = {:d}".format(a, op[0], b, add(a, b)))
     elif(sys.argv[2] is op[1]):
-        print("{:d} {:s} {:d} = {:d}".format(num1, op[1], num2, sub(num1, num2)))
+        print("{:d} {:s} {:d} = {:d}".format(a, op[1], b, sub(a, b)))
     elif(sys.argv[2] is op[2]):
-        print("{:d} {:s} {:d} = {:d}".format(num1, op[2], num2, mul(num1, num2)))
+        print("{:d} {:s} {:d} = {:d}".format(a, op[2], b, mul(a, b)))
     elif(sys.argv[2] is op[3]):
-        print("{:d} {:s} {:d} = {:d}".format(num1, op[3], num2, div(num1, num2)))
+        print("{:d} {:s} {:d} = {:d}".format(a, op[3], b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
