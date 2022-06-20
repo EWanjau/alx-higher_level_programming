a8fe55c1593a812c4f3aff04aed9c3baba1a3357
EWanjau <wamwanjau@gmail.com>
2022-06-20 14:09:57 +0000

    first commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,6 +3,6 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list:
-            print(my_list[i])
+            print(":d{}".format(my_list[i]))
     except IndexError:
         print("The No of elements exceed the index in list")
