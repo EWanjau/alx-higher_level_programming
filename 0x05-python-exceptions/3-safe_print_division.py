8981947b8381c9b7219887d7e532251b0b7f9634
EWanjau <wamwanjau@gmail.com>
2022-06-20 16:42:18 +0000

    division error handing

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError:
+    except (ZeroDivisionError, FloatingPointError):
         result = None
     finally:
         print("Inside Result: {}".format(result))
