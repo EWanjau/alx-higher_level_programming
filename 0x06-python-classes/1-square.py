d7a398aacd23e521d40e9e4a9a5bba1242d8c7c7
EWanjau <wamwanjau@gmail.com>
2022-06-21 19:05:46 +0300

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square():
+    """ class square defines a square"""
+    def __init__(self, size):
+        """Initialization of the unique squares"""
+        self.__size = size
