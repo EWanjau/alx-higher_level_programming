a972c5dbeaf0f9d248c939cd32a2e5a38bbc1186
EWanjau <wamwanjau@gmail.com>
2022-06-21 10:07:41 +0000

    raise exception with message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    raises an exception with a message
+    args:
+        message: the text
+    return: exception raised
+    """
+    raise NameError(message)
