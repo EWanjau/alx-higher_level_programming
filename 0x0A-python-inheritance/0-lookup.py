ae4fc9c1272283e732d4a3842561f0e0b4be97ed
EWanjau <wamwanjau@gmail.com>
2022-07-05 11:51:01 +0300

    update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-
+"""Module 0-lookup
+find a ist of attributes and methods
+"""
 def lookup(obj):
-    """returns list of available atrributes and methods of an object"""
+    """returns list of available atrributes and methods of an object
+        
+        args:
+            - obj: object to look 
+    """
     return dir(obj)
