8eb8b0483e5ae18e238c6485f57021e644971194
EWanjau <wamwanjau@gmail.com>
2022-07-07 16:23:06 +0300

    update the module description

---
+++ 0x0B-python-input_output/0-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from turtle import clear
+
+
+read_file = __import__('0-read_file').read_file
+
+read_file("my_file_0.txt")
--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""Module 0-read_file.
+Reads from a file and prints.
+"""
+
 
 def read_file(filename=""):
-    """Reads the file passed as an argument"""
+    """Reads the file passed as an argument
+
+    Args:
+        - filename: file to be opened
+    """
+
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
