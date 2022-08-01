c2e2a2be75bdea68834105b63ee6c2f592d64c14
EWanjau <wamwanjau@gmail.com>
2022-08-01 19:55:45 +0300

    created new task, add two integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python3
+""" this module adds two numbers
+and tests the inputs
+"""
+
+
+def add_integer(a, b=98):
+    """function for the addition of the two numbers"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer or b must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("a must be an integer or b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a + b)
