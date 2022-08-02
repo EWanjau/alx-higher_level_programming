a92e1f24909ed7305d8a14a11637be73d1eec50c
EWanjau <wamwanjau@gmail.com>
2022-08-02 23:24:41 +0300

    created module that prints a square and the testing conditions

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" this module has function that prints
+a square using hash symbol
+"""
+
+
+def print_square(size):
+    """prints square
+    args:
+        size: the dimensions of the square
+    result: square
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer") 
+    for i in range(size):
+        print("#" * size)
