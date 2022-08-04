48b53be3f49b4330c01cbfb350291dd8d8cad4b8
EWanjau <wamwanjau@gmail.com>
2022-08-04 23:53:35 +0300

    added validation checks to setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,16 +12,30 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """class constructor"""
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
 
     @property
     def width(self):
         """get the value of the width """
         return self.__width
+
+    @property
+    def height(self):
+        """retrieve the height"""
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     @width.setter
     def width(self, value):
@@ -32,11 +46,6 @@ class Rectangle(Base):
             raise ValueError("width must be > 0")
         self.__width = value
 
-    @property
-    def height(self):
-        """retrieve the height"""
-        return self.__height
-
     @height.setter
     def height(self, value):
         """set the height"""
@@ -46,10 +55,6 @@ class Rectangle(Base):
             raise ValueError("height must be > 0")
         self.__height = value
 
-    @property
-    def x(self):
-        return self.__x
-
     @x.setter
     def x(self, value):
         if type(value) is not int:
@@ -57,10 +62,6 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("x must be > 0")
         self.__x = value
-
-    @property
-    def y(self):
-        return self.__y
 
     @y.setter
     def y(self, value):
