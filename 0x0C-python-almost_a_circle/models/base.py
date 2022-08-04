5b18899bcb0e6501fed5cd67eb9ba523fcbdb2af
EWanjau <wamwanjau@gmail.com>
2022-08-04 20:08:33 +0300

    created a base class with private class attribute

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class that defines a Base"""
+
+
+class Base:
+    """the attributes and methods 
+    defining the class
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
