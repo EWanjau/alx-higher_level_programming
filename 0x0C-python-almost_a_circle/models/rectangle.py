da8238f48b7918259637b87ca016fe9ef667f8d6
EWanjau <wamwanjau@gmail.com>
2022-08-05 09:54:55 +0300

    created a pulic class method update that takes variable arguments and assigns their id

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,6 @@
 """class that defines rectangle"""
 
 
-from curses.textpad import rectangle
 from models.base import Base
 
 
@@ -93,3 +92,17 @@ class Rectangle(Base):
         """print the objects of the class"""
         return "[Rectangle] ({}) {}/{} - {}/{}" .\
             format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        """assigns an argument to each attribute"""
+
+        self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+        
