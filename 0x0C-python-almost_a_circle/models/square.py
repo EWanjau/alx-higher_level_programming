793d1d7d464e6bbe52c8b3431535576cadc68e76
EWanjau <wamwanjau@gmail.com>
2022-08-15 10:29:43 +0300

    created an update method to class square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,7 +13,7 @@ class Square(Rectangle):
 
     def __str__(self):
         return print("[Square] ({}) {}/{} - {}".
-                     format(self.id, self.x, self.y, self.size))
+                     format(self.id, self.x, self.y, self.__width))
 
     @property
     def size(self):
@@ -30,3 +30,28 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.__width = value
         self.__height = value
+
+    def update(self, *args, **kwargs):
+        """assign attributes"""
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if type(value) != int and value is not None:
+                        raise TypeError("id must be an integer")
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
