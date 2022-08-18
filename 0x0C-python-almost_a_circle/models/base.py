09ec95d6f81fc723f3ed7d6ebacc3fed4908193a
EWanjau <wamwanjau@gmail.com>
2022-08-18 08:02:57 +0300

    add static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,6 +19,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """display json rep of all dictionaries"""
         if list_dictionaries is None and list_dictionaries == []:
