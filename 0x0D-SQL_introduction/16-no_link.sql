4f396ad3bd8a6e0930f5fe7e491305562844c3ca
EWanjau <wamwanjau@gmail.com>
2022-08-29 12:22:08 +0300

    list all records apart from missing ones

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- a script that lists all records apart from the missing fields
-SELECT * FROM second_table WHERE name != NULL ORDER BY score DESC
+SELECT score, name  FROM second_table WHERE name IS NOT NULL ORDER BY score DESC
