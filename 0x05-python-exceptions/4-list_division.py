be41604896b11f9be3e7f30ead0b0095f54ed9b1
EWanjau <wamwanjau@gmail.com>
2022-09-12 18:41:45 +0300

    re initialized the quotient for every exception

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,10 +18,13 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[val] / my_list_2[val]
         except ZeroDivisionError:
+            result = 0
             print("division by 0")
         except TypeError:
+            result = 0
             print("wrong type")
         except IndexError:
+            result = 0
             print("out of range")
         finally:
             length.append(result)
