6e2505d0dec681e7a027066af81b5ec5e53f96b2
EWanjau <wamwanjau@gmail.com>
2022-09-12 18:31:51 +0300

    removed the loop to start from the beginning

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,11 +8,12 @@ def safe_print_list_integers(my_list=[], x=0):
             output:
                 list of integers
     """
-    try:
-        for val in range(0, x):
-            if isinstance(my_list[val], int):
-                print("{:d}".format(my_list[val]), end="")
-        print()
-        return x
-    except (IndexError):
-        print()
+    count = 0
+    for val in range(0, x):
+        try:
+            print("{:d}".format(my_list[val]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return count
