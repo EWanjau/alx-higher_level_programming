14f21ba7b9643d9072f2c79a63ef94ec5da0d393
EWanjau <wamwanjau@gmail.com>
2022-11-02 10:10:37 +0300

    added the fields on the table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- script that adda a second table
 -- create records and store in them
 CREATE TABLE IF NOT EXISTS  second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES
+INSERT INTO second_table (id, name, score) VALUES
 (1, "John", 10),
 (2 "Alex", 3),
 (3, "Bob", 14),
