b065926df7bf9c9e83e0156dcd36a84e27ccd0f5
EWanjau <wamwanjau@gmail.com>
2022-11-03 23:00:25 +0300

    corrected error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- created a new user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
