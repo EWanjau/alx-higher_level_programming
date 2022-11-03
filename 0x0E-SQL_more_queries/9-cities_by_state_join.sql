d834558553686c30d138bdd1eb2a5ed1d343eac0
EWanjau <wamwanjau@gmail.com>
2022-11-03 23:14:41 +0300

    corrected error

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- join two tables and get a mysql query
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.id=states.id;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id;
