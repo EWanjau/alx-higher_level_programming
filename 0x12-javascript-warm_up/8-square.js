533a2d737423be3c070f9f6e2149646067a64309
EWanjau <wamwanjau@gmail.com>
2022-11-29 00:00:24 +0300

    corrections handing

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-if (parseInt(process.argv[2])) {
-  for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    for (let j = 0; j < parseInt(process.argv[2]); j++) { process.stdout.write('X'); }
-    console.log(' ');
-  }
-} else {
+const num = Number.parseInt(process.argv[2]);
+if (!num) {
   console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++)
+    console.log('X'.repeat(num));
 }
