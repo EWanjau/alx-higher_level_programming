a1e869928d433eedeba11e485c26422f1b0bcef1
EWanjau <wamwanjau@gmail.com>
2022-11-28 20:01:44 +0300

    added script to print array of strings

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// using arrays and a loop
+const array1 = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < array1.length; i++) {
+  console.log(array1[i]);
+}
