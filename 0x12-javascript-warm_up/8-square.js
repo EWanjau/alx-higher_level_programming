8b2fbb11293ce2d462e33d3f86ebfa03efb47f2f
EWanjau <wamwanjau@gmail.com>
2022-11-28 20:45:13 +0300

    added script to print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// prints a square
+if (parseInt(process.argv[2])) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    for (let j = 0; j < parseInt(process.argv[2]); j++) { process.stdout.write('X'); }
+    console.log(' ');
+  }
+} else {
+  console.log('Missing size');
+}
