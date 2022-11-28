b8ac32929830ed7428f8194ec964d0c7010cd918
EWanjau <wamwanjau@gmail.com>
2022-11-28 20:13:26 +0300

    added script to iterate on cmd line input integer

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script that prints a string n times
+if (parseInt(process.argv[2])) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
