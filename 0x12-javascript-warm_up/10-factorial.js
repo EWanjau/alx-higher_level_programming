886b5878d2b9b882afd87f6a545dc771c5035697
EWanjau <wamwanjau@gmail.com>
2022-11-29 00:05:59 +0300

    corrections handing

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
+const a = Number.parseInt(process.argv[2]);
 function factorial (a) {
-  a = parseInt(process.argv[2]);
   if (!a) {
     return 1;
   }
   return (a * factorial(a - 1));
 }
 
-console.log(factorial());
+console.log(factorial(a));
