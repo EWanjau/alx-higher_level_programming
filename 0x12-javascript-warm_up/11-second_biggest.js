f82c30b80e3d0174476c7357b5a6135f5231597a
EWanjau <wamwanjau@gmail.com>
2022-11-29 00:13:00 +0300

    corrections handing

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2] == null) {
+const array = process.argv.slice(2).map(element => Number.parseInt(element));
+if (array.length < 2) {
   console.log(0);
-} else if (process.argv.length == 3) {
-  console.group(0);
 } else {
-  let max_num = process.argv[2];
-  for (let i = 0; i < process.argv.length; i++) {
-    if (process.argv[i] > max_num) { max_num = process.argv[i]; }
-    diff = max_num - process.argv[i];
-  }
+  array.sort((a, b) => a - b);
+  console.log(array[array.length - 2]);
 }
