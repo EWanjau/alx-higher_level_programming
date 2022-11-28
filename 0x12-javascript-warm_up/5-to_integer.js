f7e17273e10ff6945100539659b559b264ad9396
EWanjau <wamwanjau@gmail.com>
2022-11-28 16:29:56 +0300

    added a script that converts input to integers

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // a function that uses try and catch to print
 if (Number(process.argv[2])) {
-  console.log('My number: ' + Number(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 } else {
   console.log('Not a number');
 }
