9f7d23698ffaa3bb37734ea3a5b9b115c827efdc
EWanjau <wamwanjau@gmail.com>
2022-12-05 14:13:56 +0300

    included the shebang line

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,15 @@
+#!/usr/bin/python3
 """0-select_states connects using an ORM and manipulates
 mysql
 """
 
+
+import sys
+import MySQLdb
+
 if __name__ == "__main__":
     """connects to the database and selects records from a table"""
-    import sys
-    import MySQLdb
-    db = MySQLdb.connect(host=sys.argv[1], user=sys.argv[2], passwd=sys.argv[3], db=sys.argv[4])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM `states`")
     [print(state) for state in cur.fetchall()]
