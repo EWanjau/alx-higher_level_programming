1f327c8e1bc48282deedc4016090df89fb4564d3
EWanjau <wamwanjau@gmail.com>
2022-12-07 18:37:28 +0300

    added a script taht combines two tables

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""0-select_states connects using an ORM and manipulates
+the database mitigating risk from SQL injections
+"""
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT c.id, c.name , s.name \
+        FROM cities as c \
+            INNER JOIN states as s \
+            ON c.state_id = s.id \
+            ORDER BY c.id")
+    [print(city) for city in c.fetchall()]
