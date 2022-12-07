cff24fba66f682f66a06adca8f2f60b42dbaf5fc
EWanjau <wamwanjau@gmail.com>
2022-12-07 20:50:03 +0300

    added script that fetches list from table using sql alchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Start link class objects to table in database
 """
-import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
+from sys import argv
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:\
-                           {}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
 
     session = Session(engine)
