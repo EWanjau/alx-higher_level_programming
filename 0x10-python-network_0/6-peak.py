3ae7616ee85d31416dbe33da4f5d8646e215de37
EWanjau <wamwanjau@gmail.com>
2022-12-20 17:11:08 +0300

    added a script to firds  peak of an unsorted list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""this module has function that finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """the function gets the peak of unsorted list
+    input:
+            @list_of_integers
+    output:
+            highest number
+    """
+
