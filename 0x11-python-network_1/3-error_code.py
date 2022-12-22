8968554aab3175977504e6a74de4d25c4a92fa7a
EWanjau <wamwanjau@gmail.com>
2022-12-22 12:38:24 +0300

    added a script that handles error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     try:
         with request.urlopen(argv[1]) as response:
-            page = response.rea().decode("utf-8")
+            page = response.read().decode("utf-8")
         print(page)
     except error.HTTPError as error:
         print("Error code: {}".format(error.code))
