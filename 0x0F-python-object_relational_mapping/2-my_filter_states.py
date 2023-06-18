162f31ffd469398f4d6e3f00fabd87cd7dcb518b
EWanjau <wamwanjau@gmail.com>
2023-06-18 18:59:29 +0300

    script selects a specific record from a table

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this module connects using an ORM and manipulates
+mysql using msqldb module
+"""
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    """connects to the database and selects records from a table which match an argument"""
+    db=MySQLdb.connect(user=argv[1],passwd=argv[2],db=argv[3])
+    c=db.cursor()
+    c.execute("SELECT * FROM states WHERE name='arv[4]' ORDER BY `state.id` ASC")
+    [print(state) for state in c.fetchall()]
