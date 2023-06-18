74f7ae564f598246f900fc5d13e2cd6f6ad7704b
EWanjau <wamwanjau@gmail.com>
2023-06-18 19:16:22 +0300

    added pep8 styling to files

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ import MySQLdb
 
 if __name__ == "__main__":
     """connects to the database and selects records from a table"""
-    db=MySQLdb.connect(user=sys.argv[1],passwd=sys.argv[2],db=sys.argv[3])
-    c=db.cursor()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
     c.execute("SELECT * FROM `states` ORDER BY `state.id` ASC")
     [print(state) for state in c.fetchall()]
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,10 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    """connects to the database and selects records from a table which match an argument"""
-    db=MySQLdb.connect(user=argv[1],passwd=argv[2],db=argv[3])
-    c=db.cursor()
-    c.execute("SELECT * FROM states WHERE name='arv[4]' ORDER BY `state.id` ASC")
+    """connects to the database and selects records from a
+    table which match an argument"""
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name='arv[4]'\
+        ORDER BY `state.id` ASC")
     [print(state) for state in c.fetchall()]
