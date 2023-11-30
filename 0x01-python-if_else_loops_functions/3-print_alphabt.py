d71ffe225a3ea5676f7424a2167800c605aaf48e
EWanjau <wamwanjau@gmail.com>
2023-11-30 17:09:10 +0300

    added corrections to the files

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if(i != 101):
-        if(i != 113):
+    if (i != 101):
+        if (i != 113):
             print("{0:c}".format(i), end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in str:
         i = ord(i)
-        if(i >= 97 and i < 123):
+        if (i >= 97 and i < 123):
             i = i - 32
         print("{:s}".format(chr(i)), end="")
     print()
