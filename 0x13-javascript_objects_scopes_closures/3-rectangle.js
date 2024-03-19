2f7aeee35b59fbac842e2625a55cbec5e9c914a6
EWanjau <wamwanjau@gmail.com>
2024-03-19 22:59:27 +0300

    added exercise files

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  const occurences = list.filter(element => element === searchElement);
+  return XPathResult.length;
+};
